de782f6f24ed1cd6447d2822ada423ec8ddbd514
Tuysh <angelmacloviomorales@gmail.com>
2025-06-13 16:50:08 -0600

    [DOC] Agregar documentación a la excepción NormalException

--- src/common/exceptions/normal.exception.ts
+++ src/common/exceptions/normal.exception.ts
@@ -1,5 +1,17 @@
 import { HttpException } from '@nestjs/common';
 
+/**
+ * Excepción personalizada para cuando algo sale mal.
+ *
+ * Devuelve un error HTTP 418 (I'm a teapot) con un mensaje humorístico
+ * relacionado con Bocchi, personaje de anime conocido por su ansiedad social.
+ * Si, tuve que documentar esto de la forma más seria posible... Ignoren esto...
+ * No me juzguen por esto.
+ *
+ * @example
+ * throw new NormalException();
+ * // Respuesta: { error: 'Bocchi está demasiado nerviosa...' } (status 418)
+ */
 export class NormalException extends HttpException {
   constructor() {
     super(
